926bb56dfc69e56200fca395170fa31172989747
alfisar <alfisar589@gmail.com>
2019-04-21 15:09:36 +0700

    fix plot

--- Tupro2.py
+++ Tupro2.py
@@ -1,4 +1,4 @@
-import numpy
+from numpy import *
 import csv
 import random
 import math
@@ -13,51 +13,39 @@ def jarak(x,n):
 # Menghitung jarak neuron pemenang terhadap 2 tetangganya
 def hitungsn(x):
     tempsn = []
-    j = 2 
-    for i in range(3): 
-        tempsn.append(math.sqrt(((x[0]-n[x[j]][0])**2) + ((x[1]-n[x[j]][1])**2)))
-        j+=1
+
+    for i in range(10):
+        hasil = math.sqrt(((x[0]-n[i][0])**2) + ((x[1]-n[i][1])**2))
+        # print(hasil)
+        if (hasil < sigma) :
+            tempsn.append([i,hasil])
+
     return tempsn
 
 # Menghitung Tn 
 def hitungtn(x):
     temptn = []
-    for i in range(3):
-        temptn.append(math.exp(-(x[i]**2)/(2*(sigma**2))))
+    for i in range(len(x)):
+        temptn.append(double(math.exp(-(x[i][1]**2)/(2*(sigma**2)))))
     return temptn
 
 # Menghitung Wn untuk di tambahkan pada Weight neuronnya 
 def hitungwn(x,y):
-    temp3,temptetangga = [],[]
-    l = tempjar[0][1]
-    j = 2
-    for i in range(3):
-        temptetangga.append(n[l][j])
-        j+=1
-
-    for j in range(3):
+    temp3 = []
+    for j in range(len(y)):
         temp1 = []
         for i in range (2):
-            temp1.append(str(lr*y[j]*(x[i]-n[temptetangga[j]][i])))
+            temp1.append(lr*y[j]*(x[i]-n[tempsn[j][0]][i]))
         temp3.append((temp1[0],temp1[1]))
     # print(temp3)
     return temp3
 
 # Menambahkan Wn dengan weight neuronnya
 def gantiw(x):
-    temptetangga= []
-    l = tempjar[0][1]
-    j = 2
-    for i in range(3):
-        temptetangga.append(n[l][j])
-        j+=1
-    # print("tetangga")
-    # print(temptetangga)
-    # print(x)
     for i in range(len(x)):
         k = []
         for j in range(2):
-            n[temptetangga[i]][j] = n[temptetangga[i]][j] + float(x[i][j][:3])
+            n[tempsn[i][0]][j] = n[tempsn[i][0]][j] + x[i][j]
     return k
 
 def takeSecond1(elem):
@@ -66,16 +54,14 @@ def takeSecond1(elem):
 f = open("Tugas 2 ML Genap_2018-2019_Dataset_Tanpa_Label.csv","r")
 reader = csv.reader(f)
 
-lis1,n = [],[]
+lis1,n,tetangga = [],[],[]
 for d in reader:
     lis1.append((float(d[0]),float(d[1])))
 
 # Membangkitakan 4 neuron dengan weightnya di random dan tetangganya di tentukan sendiri
-n.append([random.uniform(-20,20),random.uniform(-20,20),0,1,2])
-n.append([random.uniform(-20,20),random.uniform(-20,20),1,0,3])
-n.append([random.uniform(-20,20),random.uniform(-20,20),2,0,3])
-n.append([random.uniform(-20,20),random.uniform(-20,20),3,1,2])
-
+for i in range(10):
+    n.append([random.uniform(-15,15),random.uniform(-15,15)])
+print('n awal')
 print(n)
 
 sigma0 = 2
@@ -86,8 +72,9 @@ tn = 2
 sigma,lr = 0,0
 
 # Perulangan untuk iterasi
-for a in range(5):
+for a in range(50):
     sigma = sigma0 * math.exp(-(a+1)/t0)
+    # print(sigma)
     lr = lr0 * math.exp(-(a+1)/tn)
     # Perulangan untuk setiap data
     for j in range(len(lis1)):
@@ -99,13 +86,16 @@ for a in range(5):
         tempsn = hitungsn(n[tempjar[0][1]])
         # print(tempsn)
         temptn = hitungtn(tempsn)
+        # print('tn')
         # print(temptn)
         temp1 = hitungwn(lis1[j],temptn)
+        # print('wn')
+        # print(temp1)
         k = gantiw(temp1)
-    
+print('n akhir')    
 print(n)
 
-hasil_final,n0_1,n0_2,n1_1,n1_2,n2_1,n2_2,n3_1,n3_2 = [],[],[],[],[],[],[],[],[]
+coba = []
 
 # Perulangan untuk setiap data
 for j in range(len(lis1)):
@@ -115,36 +105,33 @@ for j in range(len(lis1)):
         tempjar.append((jarak(lis1[j],n[i]),i))
         tempjar.sort(key=takeSecond1)
     
-    if tempjar[0][1] == 0:
-        n0_1.append(lis1[j][0])
-        n0_2.append(lis1[j][1])
-    elif tempjar[0][1] == 1:
-        n1_1.append(lis1[j][0])
-        n1_2.append(lis1[j][1]) 
-    elif tempjar[0][1] == 2:
-        n2_1.append(lis1[j][0])
-        n2_2.append(lis1[j][1])
-    elif tempjar[0][1] == 3:
-        n3_1.append(lis1[j][0])
-        n3_2.append(lis1[j][1])   
+    coba.append([tempjar[0][1],lis1[j][0],lis1[j][1]])
 
-# print(hasil_final[1])
+# print(coba)
 
 data1,data2 = [],[]
 j = 0
-for i in (lis1):
-    j+=1
-    data1.append(i[0])
-    data2.append(i[1])
-
-plt.scatter(n0_1, n0_2, c ='purple', alpha=1)
-plt.scatter(n1_1, n1_2, c ='g', alpha=1)
-plt.scatter(n2_1, n2_2, c ='b', alpha=1)
-plt.scatter(n3_1, n3_2, c ='c', alpha=1)
-plt.scatter(n[0][0], n[0][1], marker = 'x', label = 0, c='purple', alpha=1)
-plt.scatter(n[1][0], n[1][1], marker = '^', c='g', alpha=1)
-plt.scatter(n[2][0], n[2][1], marker = '*', c='b', alpha=1)
-plt.scatter(n[3][0], n[3][1], marker = 's', c='c', alpha=1)
+for i in (coba):
+    if (i[0] == 0):
+        plt.scatter(i[1], i[2], c ='purple', alpha=1)
+    elif (i[0] == 1):
+        plt.scatter(i[1], i[2], c ='g', alpha=1)
+    elif (i[0] == 2):
+        plt.scatter(i[1], i[2], c ='b', alpha=1)
+    elif (i[0] == 3):
+        plt.scatter(i[1], i[2], c ='black', alpha=1)
+    elif (i[0] == 4):
+        plt.scatter(i[1], i[2], c ='pink', alpha=1)
+    elif (i[0] == 5):
+        plt.scatter(i[1], i[2], c ='red', alpha=1)
+    elif (i[0] == 6):
+        plt.scatter(i[1], i[2], c ='c', alpha=1)
+    elif (i[0] == 7):
+        plt.scatter(i[1], i[2], c ='brown', alpha=1)
+    elif (i[0] == 8):
+        plt.scatter(i[1], i[2], c ='yellow', alpha=1)
+    elif (i[0] == 9):
+        plt.scatter(i[1], i[2], c ='orange', alpha=1)
 plt.title('Scatter plot SOM')
 plt.xlabel('x')
 plt.ylabel('y')
